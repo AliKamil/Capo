ea72b3154de341bf70fb73f1efa8001bb4be9259
Arthur Dombrovskii <pandombrovski@gmail.com>
2016-02-25 00:18:46 +0200

    Feature(actionParse): added bulk fetch, all refactored

--- commands/ParserController.php
+++ commands/ParserController.php
@@ -1,148 +1,151 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Admin
- * Date: 24.02.2016
- * Time: 12:28
+ * Created by Arthur Dombrovskii
+ * Capo positions parser for UltimateGuitar.com
  */
 
 namespace app\commands;
 
-use Faker\Provider\zh_TW\DateTime;
 use PDOException;
 use yii\console\Controller;
 use yii\db\mssql\PDO;
+use yii\helpers\Console;
 
 class ParserController extends Controller
 {
-    //hardcoding database access data for speed - it's not a good practice
-    protected
-        $source = ['host' => 'devreplica.lan',
-                   'usr' => 'testguitar',
-                   'pwd' => 'zzVzz5317B',
-                   'port' => '3303'
-    ], $dest = ['host' => '192.168.14.243',
-                'usr' => 'testguitar',
-                'pwd' => 'zzVzz5317B',
-                'port' => '3307'
-    ], $limit = 100000,
-        $bulk_size = 1000,
-        $statistic = [],
-        $numerals = ['first',
-                     'second',
-                     'third',
-                     'fourth',
-                     'fifth',
-                     'sixth',
-                     'seventh',
-                     'eighth',
-                     'ninth',
-                     'tenth',
-                     'eleventh',
-                     'twelfth'];
+    protected $numerals = ['first',
+                           'second',
+                           'third',
+                           'fourth',
+                           'fifth',
+                           'sixth',
+                           'seventh',
+                           'eighth',
+                           'ninth',
+                           'tenth',
+                           'eleventh',
+                           'twelfth'];
+    /** @var PDO $source */
+    protected $source;
+    /** @var PDO $destination */
+    protected $destination;
 
     private $startTime;
 
-
+    /**
+     * Main loop
+     */
     public function actionParse()
     {
-        $this->start();
+        $this->source = $this->getConnection(
+            ['host' => 'devreplica.lan',
+             'usr' => 'testguitar',
+             'pwd' => 'zzVzz5317B',
+             'port' => '3303'
+            ]
+        );
 
-        $regexp = "/capo\\D{0,10}(\\d{1,2}|" . implode('|', $this->numerals) . ")/is";
-        $no_capo_regexp = "/no +capo/is";
-
-        $source = $this->getConnection($this->source);
-        $destination = $this->getConnection($this->dest);
-
-        $query = $source->prepare('SELECT content, id from content.tabs limit ' . $this->limit);
-        $query->execute();
-
-        $values = [];
-
-        while ($result = $query->fetch(PDO::FETCH_ASSOC)) {
-            $data = $result['content'];
-            $id = $result['id'];
-
-            //removing "no capo" entries
-            $data = preg_replace($no_capo_regexp, '', $data);
-            preg_match_all($regexp, $data, $matches);
-            if (!empty($matches[0])) {
-                $position = $matches[1][0]; //we deal only with first occurence
-                if (!is_numeric($position)) {
-                    $position = array_search($position, $this->numerals);
-                }
-                if ($position > 0) {
-                    $values[] = [$id,
-                                 $position];
-                }
-                if (count($values) == $this->bulk_size) {
-
-                    $this->insert($destination, $values);
-                    $values = [];
-                }
-            }
-        }
-        $this->insert($destination, $values);
-        var_dump($this->stop());
-    }
-
-    public function actionGetStatistic()
-    {
-        $numeral = "/capo.{0,10}(first|second|third|fourth|fifth|sixth|seventh|eighth|ninth|tenth|eleventh|twelfth)/is";
-        $digital = "/capo.{0,10}\\d{1,2}/is";
-        $digital_matches = 0;
-        $numeral_matches = 0;
-        $other = 0;
-        $total = 0;
-        $capo_matches = [];
+        $this->destination = $this->getConnection(
+            ['host' => '192.168.14.243',
+             'usr' => 'testguitar',
+             'pwd' => 'zzVzz5317B',
+             'port' => '3307'
+            ]
+        );
+        $limit = 1000;
+        $offset = 0;
 
         $this->start();
-        $connection = $this->getConnection($this->source);
-        $file = fopen('statistic', 'w');
-        if ($file === false) {
-            var_dump('panic');
-            die;
-        }
-        $query = $connection->prepare('SELECT content from content.tabs limit ' . $this->limit);
-        $query->execute();
-        while ($result = $query->fetch(PDO::FETCH_ASSOC)) {
-            $data = $result['content'];
-            $t = preg_match_all("/capo/i", $data, $tm);
-            $total += $t;
-            $data = preg_replace($numeral, '', $data, -1, $n_matches);
-            $data = preg_replace($digital, '', $data, -1, $d_matches);
-            $other += preg_match_all('/.{0,15}capo.{0,15}/i', $data, $matches);
-            $digital_matches += $d_matches;
-            $numeral_matches += $n_matches;
-            if (is_array($matches[0])) {
-                $capo_matches = array_merge($capo_matches, $matches[0]);
-            }
-        }
+        do {
+            //retrieving data from source
+            $data = $this->getData($limit, $offset);
+            //filtering data
+            $result = $this->filter($data);
+            //inserting result to destination
+            $this->insert($result);
 
-        fwrite($file, 'd:' . $digital_matches . "\r\n");
-        fwrite($file, 'n:' . $numeral_matches . "\r\n");
-        fwrite($file, 'o:' . $other . "\r\n");
-        fwrite($file, 't:' . $total . "\r\n");
-        fwrite($file, json_encode($capo_matches));
+            $offset += $limit;
+        } while (!empty($data));
         var_dump($this->stop());
     }
 
     /**
-     * @param PDO $connection
-     * @param $data
+     * Bulk fetch data from source database
+     *
+     * @param int $limit
+     * @param null $offset
+     * @return array
      */
-    protected function insert(PDO $connection, $data)
+    protected function getData($limit = 1000, $offset = null)
+    {
+        $query_string = 'SELECT t.content, t.id from content.tabs as t';
+        if ($offset) {
+            $query_string .= sprintf(
+                ' JOIN (SELECT id FROM content.tabs ORDER BY id LIMIT %d, %d) as i ON i.id = t.id',
+                $offset,
+                $limit
+            );
+        } else {
+            $query_string .= sprintf(' LIMIT %d', $limit);
+        }
+        $query = $this->source->prepare($query_string);
+        $query->execute();
+        return $query->fetchAll();
+    }
+
+    /**
+     * Finding capo pos using regexps
+     *
+     * @param array $data
+     * @return array
+     */
+    protected function filter($data)
+    {
+        $result = [];
+        $regexp = "/capo\\D{0,10}(\\d{1,2}|" . implode('|', $this->numerals) . ")/is";
+        $no_capo_regexp = "/no +capo/is";
+
+        foreach ($data as $tab) {
+            //first removing all 'no capo entries'
+            $tab = preg_replace($no_capo_regexp, '', $tab);
+            //finding matches
+            preg_match_all($regexp, $tab, $matches);
+            if (!empty($matches[0])) {
+                $position = $matches[1][0]; //we deal only with first occurence, maybe TODO: is it a good practice?
+                if (!is_numeric($position)) {
+                    $position = array_search($position, $this->numerals);
+                }
+                if ($position > 0) {
+                    $result[] = [$tab['id'],
+                                 $position];
+                }
+            }
+        }
+
+        return $result;
+    }
+
+
+    /**
+     * Bulk insert data
+     *
+     * @param $data
+     * @return int|Boolean
+     * @internal param PDO $connection
+     */
+    protected function insert($data)
     {
         $query = 'INSERT INTO test.result (tab_id, capo_value) VALUES ';
         foreach ($data as $d) {
             $query .= '(' . $d[0] . ',' . $d[1] . '),';
         }
-        $connection->exec(rtrim($query, ','));
-        var_dump($connection->errorInfo());
+        return $this->destination->exec(rtrim($query, ','));
+
     }
 
     /**
-     * @return bool|PDO
+     * @param array $cfg
+     * @return PDO
      */
     protected function getConnection($cfg)
     {
@@ -156,21 +159,52 @@ class ParserController extends Controller
             );
             return $connection;
         } catch (\PDOException $err) {
-            var_dump($err->getMessage());
-            return false;
+            $this->stdout($err->getMessage(), Console::BG_RED);
+            die;
         }
     }
 
+    /**
+     * The following part is for perfomance testing purposes only
+     */
+
+
+    /**
+     * Start time count
+     */
     protected function start()
     {
         $date = new \DateTime();
         $this->startTime = $date->getTimestamp();
     }
 
+    /**
+     * Stop time count and returns script executing time
+     * @return int
+     */
     protected function stop()
     {
         $date = new \DateTime();
         return $date->getTimestamp() - $this->startTime;
+    }
+
+    /**
+     * SpeedTest
+     */
+    public function actionTest()
+    {
+        $this->source = $this->getConnection(
+            ['host' => 'devreplica.lan',
+             'usr' => 'testguitar',
+             'pwd' => 'zzVzz5317B',
+             'port' => '3303'
+            ]
+        );
+        $this->start();
+        $query = 'select content, id from content.tabs where id = 1000';
+        $query = $this->source->prepare($query);
+        $query->execute();
+        $this->stdout(sprintf('Selecting one row by id took: %d', $this->stop()));
     }
 
 }
